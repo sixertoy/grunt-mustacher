87cb5296a96cc5b467cae159317c4423ccf7bf6e
malas34 <malas34.github@gmail.com>
2014-07-22 12:08:14 +0200

    Ajout des tests unitaires
    Utils.isEmpty
    Utils.isString
    Utils.isObject

--- tests/grunt-mustacher-utils_test.js
+++ tests/grunt-mustacher-utils_test.js
@@ -8,67 +8,95 @@ exports.MustacherUtilsTest = {
         return done();
     },
     isJSONContext: function (test) {
-        test.expect(1);
-        var result, expected;
-        // ok
+        test.expect(5);
+        var expected;
         expected = true;
-        result = this.utils.isJSONContext('{"toto":"1"}');
-        test.equal(result, expected, 'mustacherUtils.isJSONContext({"toto":"1"}) test failed.');
         // ok
-        /*
-        expected = true;
-        result = this.utils.isJSONContext('{toto:0}');
-        test.equal(result, expected, 'mustacherUtils.isJSONContext({toto:0}) test failed.');
-        */
+        test.equal(this.utils.isJSONContext('{"toto":"1"}'), expected, 'mustacherUtils.isJSONContext({"toto":"1"}) test failed.');
+        // ok
+        test.equal(this.utils.isJSONContext('{"toto":0}'), expected, 'mustacherUtils.isJSONContext({"toto":0}) test failed.');
+        // false
+        test.notEqual(this.utils.isJSONContext('{toto:0}'), expected, 'mustacherUtils.isJSONContext({toto:0}) test failed.');
         // fail
-        /*
-        expected = false;
-        result = this.utils.isJSONContext({});
-        test.equal(result, expected, 'mustacherUtils.isJSONContext({}) test failed.');
-        */
+        test.notEqual(this.utils.isJSONContext({}), expected, 'mustacherUtils.isJSONContext({}) test failed.');
         // fail
-        /*
-        expected = false;
-        result = this.utils.isJSONContext('{}');
-        test.equal(result, expected, 'mustacherUtils.isJSONContext({}) test failed.');
-        */
+        test.notEqual(this.utils.isJSONContext('{}'), expected, 'mustacherUtils.isJSONContext({}) test failed.');
         return test.done();
     },
     isObject: function (test) {
         test.expect(6);
-        var result, expected;
+        var expected;
         // ok
         expected = true;
-        result = this.utils.isObject({
-            string: '1'
-        });
-        test.equal(result, expected, 'mustacherUtils.isObject(1) OK test failed.');
+        test.equal(this.utils.isObject({
+            string: 'hello world!'
+        }), expected, 'mustacherUtils.isObject(1) OK test failed.');
         // ok
-        expected = true;
-        result = this.utils.isObject({
-            string: '1',
-            arr: ['ok', [], 2],
+        test.equal(this.utils.isObject({
+            string: 'hello world!',
+            arr: ['hello world!', [], 2],
             obj: {
                 data: 1
             }
-        });
-        test.equal(result, expected, 'mustacherUtils.isObject(obj+array) OK test failed.');
+        }), expected, 'mustacherUtils.isObject(obj+array) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isObject('{data:0}'), expected, 'mustacherUtils.isObject({data:0}) FAIL test failed.');
+        // fail
+        test.notEqual(this.utils.isObject(['fail', [], 0]), expected, 'mustacherUtils.isObject([fail, [], 0]) FAIL test failed.');
+        // fail
+        test.notEqual(this.utils.isObject('fail'), expected, 'mustacherUtils.isObject(fail) FAIL test failed.');
+        // fail
+        test.notEqual(this.utils.isObject(5), expected, 'mustacherUtils.isObject(5) FAIL test failed.');
+        return test.done();
+    },
+    isString: function (test) {
+        test.expect(6);
+        var expected;
         // ok
         expected = true;
-        result = this.utils.isJSONContext('{toto:0}');
-        test.equal(result, expected, 'mustacherUtils.isObject({toto:0}) FAIL test failed.');
+        test.equal(this.utils.isString('hello world!'), expected, 'mustacherUtils.isObject("hello world!") OK test failed.');
+        // ok
+        test.equal(this.utils.isString(''), expected, 'mustacherUtils.isObject("") OK test failed.');
         // fail
-        expected = false;
-        result = this.utils.isJSONContext(['fail', [], 0]);
-        test.equal(result, expected, 'mustacherUtils.isObject([fail, [], 0]) FAIL test failed.');
+        test.notEqual(this.utils.isString({}), expected, 'mustacherUtils.isObject({}) OK test failed.');
         // fail
-        expected = false;
-        result = this.utils.isJSONContext('fail');
-        test.notEqual(result, expected, 'mustacherUtils.isObject(fail) FAIL test failed.');
+        test.notEqual(this.utils.isString([]), expected, 'mustacherUtils.isObject([]) OK test failed.');
         // fail
-        expected = false;
-        result = this.utils.isJSONContext(5);
-        test.equal(result, expected, 'mustacherUtils.isObject(5) FAIL test failed.');
+        test.notEqual(this.utils.isString(3), expected, 'mustacherUtils.isObject(3) OK test failed.');
+        // ok
+        test.equal(this.utils.isString(3 + ''), expected, 'mustacherUtils.isObject(3+"") OK test failed.');
+        return test.done();
+    },
+    isEmpty: function (test) {
+        test.expect(12);
+        var expected;
+        expected = true;
+        // ok
+        test.equal(this.utils.isEmpty({}), expected, 'mustacherUtils.isEmpty({}) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty({
+            data: 'hello world!'
+        }), expected, 'mustacherUtils.isEmpty({data:"hello world!"}) OK test failed.');
+        // ok
+        test.equal(this.utils.isEmpty(''), expected, 'mustacherUtils.isEmpty("") OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty('lorem ipsum dolor sit amet'), expected, 'mustacherUtils.isEmpty("lorem ipsum dolor sit amet") OK test failed.');
+        // fail
+        test.equal(this.utils.isEmpty('   '), expected, 'mustacherUtils.isEmpty("   ") OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty(' lorem '), expected, 'mustacherUtils.isEmpty(" lorem ") OK test failed.');
+        // ok
+        test.equal(this.utils.isEmpty(0), expected, 'mustacherUtils.isEmpty(0) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty(10), expected, 'mustacherUtils.isEmpty(10) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty(-10), expected, 'mustacherUtils.isEmpty(-10) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty(-10.45), expected, 'mustacherUtils.isEmpty(-10.45) OK test failed.');
+        // ok
+        test.equal(this.utils.isEmpty([]), expected, 'mustacherUtils.isEmpty([]) OK test failed.');
+        // fail
+        test.notEqual(this.utils.isEmpty([1, 2, 3]), expected, 'mustacherUtils.isEmpty([1,2,3]) OK test failed.');
         return test.done();
     }
 };
