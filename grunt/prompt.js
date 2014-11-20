795bed4dad400340f20995e2670146a74b222080
sixertoy <sixertoy.github@gmail.com>
2014-11-20 11:54:59 +0100

    Update task commit

---
+++ grunt/prompt.js
@@ -0,0 +1,57 @@
+/*jslint indent: 4 */
+/*global module, grunt, console */
+module.exports = function (grunt, options) {
+    return {
+        target: {
+            options: {
+                questions: [{
+                    type: 'list',
+                    message: 'Type',
+                    config: 'bump.type',
+                    choices: [
+                        'feat',
+                        'fix',
+                        'docs',
+                        'style',
+                        'refactor',
+                        'test',
+                        'chore'
+                    ]
+                }, {
+                    type: 'input',
+                    message: 'Scope',
+                    config: 'bump.scope',
+                    default: false, // jshint ignore:line
+                    /*jshint ignore:start */
+                    validate: function (value) {
+                        return grunt.util.kindOf(value) === 'string';
+                    }
+                    /*jshint ignore:end */
+                }, {
+                    type: 'input',
+                    message: 'Subject',
+                    config: 'bump.subject',
+                    default: false, // jshint ignore:line
+                    /*jshint ignore:start */
+                    validate: function (value) {
+                        return grunt.util.kindOf(value) === 'string';
+                    }
+                    /*jshint ignore:end */
+                }, {
+                    type: 'input',
+                    message: 'Body',
+                    config: 'bump.body',
+                    default: ''  // jshint ignore:line
+                }], then: function(results, done){
+                    options.bump = {
+                        type: results['bump.type'],
+                        body: results['bump.body'],
+                        scope: results['bump.scope'],
+                        subject: results['bump.subject']
+                    };
+                    done();
+                }
+            }
+        }
+    };
+};
